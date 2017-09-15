b26ffe86d5ad859b89af8b4a4abc329f56a95c64
Jisse Reitsma <jisse@yireo.com>
2017-09-15 15:27:20 +0200

    WHoops

---
+++ component/components/com_api/Handler/Users.php
@@ -0,0 +1,52 @@
+<?php
+namespace Api\Handler;
+
+use Api\Handler;
+
+class Users extends Handler
+{
+    protected $allowedMethods = [
+        'UsersModelUsers' => [
+            'getItems',
+        ],
+        'UsersModelUser' => [
+            'getItem',
+        ]
+    ];
+
+    protected function getMethodName($model)
+    {
+        /**
+         * @todo: Should this kind of automatic logic allowed?
+         */
+        /*
+        $modelName = get_class($model);
+        if ($modelName === 'UsersModelUser') {
+            return 'getItem';
+        }
+        */
+
+        return parent::getMethodName($model);
+    }
+
+    protected function makeMethodCallback(string $requestType, $model)
+    {
+        $items = parent::makeMethodCallback($requestType, $model);
+
+        if ($model == 'UsersModelUsers') {
+            foreach ($items as $itemIndex => $item) {
+                if (isset($item->password)) {
+                    unset($item->password);
+                }
+
+                if (isset($item->params)) {
+                    unset($item->params);
+                }
+
+                $items[$itemIndex] = $item;
+            }
+        }
+
+        return $items;
+    }
+}
